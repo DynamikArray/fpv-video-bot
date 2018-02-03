4e3fee7d2657edd9ef97f6dba00701763aa6f018
Dynamik Array <dynamikarray@gmail.com>
2018-02-03 02:08:33 -0500

    changed env port to PORT

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ client.login(process.env.discord_token);
 var express = require("express");
 var app = express();
 
-app.set("port", process.env.port || 5000);
+app.set("port", process.env.PORT || 5000);
 app.listen(app.get("port"), function() {
   console.log("Node app is running at localhost: " + app.get("port"));
 });
