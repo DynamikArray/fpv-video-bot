111bd905296c4fc987e0d0216b7c39337db724dc
Dynamik Array <dynamikarray@gmail.com>
2018-02-03 05:02:25 -0500

    REMOVED new relic added simple setInterval loop to ping app every 5 minutes.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-require("newrelic");
 var debug = require("debug");
 
 //
@@ -142,3 +141,8 @@ app.set("port", process.env.PORT || 5000);
 app.listen(app.get("port"), function() {
   console.log("Node app is running at localhost: " + app.get("port"));
 });
+
+var http = require("http");
+setInterval(function() {
+  http.get("http://fpv-video-bot.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
